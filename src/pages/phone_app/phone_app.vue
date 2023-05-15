0d7b3f39920e5aba82be7ad70c5fa0ded9583328
hui <csijun@yeah.net>
2023-05-15 21:36:31 +0800

    feat: 'phone_app'

---
+++ src/pages/phone_app/phone_app.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="top-text">以下手机银行APP可查询信用</view>
+  <view>
+    <image style="width: 100%; height: 700px" :src="phone1" />
+  </view>
+  <view>
+    <image style="width: 100%; height: 700px" :src="phone2" />
+  </view>
+  <view>
+    <image style="width: 100%; height: 700px" :src="phone3" />
+  </view>
+  <view class="text-grey text-center mt-20">*页面信息仅供参考</view>
+  <nut-button block class="my-button" type="info" :onTap="backIndex">返回首页</nut-button>
+</template>
+
+<script setup>
+  import phone1 from '../../../images/phone1.png';
+  import phone2 from '../../../images/phone2.png';
+  import phone3 from '../../../images/phone3.png';
+</script>
+
+<style>
+  .top-text {
+    margin-left: 30px;
+    margin-bottom: 20px;
+  }
+</style>
