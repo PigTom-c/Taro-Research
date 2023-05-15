a7a51b6c089508639c27c6cfa12eb60ca76b611c
hui <csijun@yeah.net>
2023-05-15 21:33:51 +0800

    feat: '路由'

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,15 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/thread_detail/thread_detail',
+    'pages/bank/bank',
+    'pages/person/person',
+    'pages/phone_app/phone_app',
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    navigationBarTextStyle: 'black',
+  },
+});
